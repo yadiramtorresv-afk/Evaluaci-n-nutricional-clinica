769868fdd3fb32368e6e049ae9c53b4ecb8eb70b
yadiramtorresv-afk <yadiramtorresv@gmail.com>
2026-07-14 01:07:17 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'nutri-calc-v15';
+const CACHE_NAME = 'nutri-calc-v19';
 const ASSETS = [
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './logo.webp'
 ];
 
 self.addEventListener('install', function(event) {
